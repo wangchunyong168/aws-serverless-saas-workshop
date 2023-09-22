fd70b512d384e628d630bf35077e1b36c2a6115d
wangchunyong168 <56460286+wangchunyong168@users.noreply.github.com>
2023-09-22 15:59:45 +0800

    Update deploy-updates.sh

--- Solution/Lab5/scripts/deploy-updates.sh
+++ Solution/Lab5/scripts/deploy-updates.sh
@@ -9,7 +9,7 @@ python3 -m pylint -E -d E0401 $(find . -iname "*.py" -not -path "./.aws-sam/*")
   
 #Deploying shared services changes
 echo "Deploying shared services changes"  
-echo Y | sam sync --stack-name serverless-saas -t shared-template.yaml --code --resource-id LambdaFunctions/CreateTenantAdminUserFunction --resource-id LambdaFunctions/ProvisionTenantFunction -u
+echo Y | sam sync --stack-name serverless-saas -t shared-template.yaml --code --resource-id LambdaFunctions/CreateTenantAdminUserFunction --resource-id LambdaFunctions/ProvisionTenantFunction
 
 cd ../scripts || exit
 ./geturl.sh
